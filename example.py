50d059db9c028c548c2e274e7e0e217db406921e
ANTO I LONAPPAN <antoidicherian@gmail.com>
2018-06-15 07:20:12 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from evidence_plotter import plotter as p
 
-# modelName file, Evidence Values, Title, ...... Please see the section in wiki 
+# modelName file, Evidence Values, Title, ...... Please see the section 'Plotter' in wiki 
 f=p('models.txt','bao+sl+h0.txt','BAO+TDSL+$H_0$',bf_y=.5, bf_x=.42,ts=12)
 f.plot()
